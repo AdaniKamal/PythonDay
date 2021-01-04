54f2f88aeb1594382afd454aa702a183d23edda4
AdaniKamal <adanikamal@gmail.com>
2021-01-04 22:01:36 +0800

    Add files via upload

---
+++ Day4/variable.py
@@ -0,0 +1,20 @@
+#Python Variables
+
+#Basic
+Dani = 24
+Jiha = Dani
+print(Jiha)
+print('-------------------------------------')
+
+#variable can’t contain a space, use _ underscore to separate words
+
+my_money=5
+print(my_money)
+print('-------------------------------------')
+
+#Variable name = can't start with number, but can use single letter (a) to Long word/sentences
+ 
+save_money = 100
+found_money = 70
+gived_money = 80
+print((save_money * 365) + found_money + gived_money)
